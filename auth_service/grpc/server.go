f9e88ec9de29e2ac94d1ee8fb163d664ff2ed080
insanXYZ <insan.nazal@gmail.com>
2025-02-12 08:21:27 +0700

    feat: nginx simple config

--- auth_service/grpc/server.go
+++ auth_service/grpc/server.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"os"
+	"time"
 
 	"github.com/go-playground/validator/v10"
+	"github.com/golang-jwt/jwt/v5"
 	authpb "github.com/insanXYZ/proto/gen/go/auth"
 	userpb "github.com/insanXYZ/proto/gen/go/user"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 type AuthServer struct {
@@ -43,6 +48,56 @@ func (s *AuthServer) Register(ctx context.Context, req *authpb.RegisterRequest) 
 
 }
 
-func (s *AuthServer) Login(context.Context, *authpb.LoginRequest) (*authpb.LoginResponse, error) {
-	return nil, nil
+func (s *AuthServer) Login(ctx context.Context, req *authpb.LoginRequest) (*authpb.LoginResponse, error) {
+	err := s.validator.Struct(req)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := s.userClient.FindUserByEmail(ctx, &userpb.FindUserByEmailRequest{
+		Email: req.Email,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	accToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":   res.User.Id,
+		"name": res.User.Username,
+		"exp":  time.Now().Add(10 * time.Second).Unix(),
+	})
+
+	refToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":   res.User.Id,
+		"name": res.User.Username,
+		"exp":  time.Now().Add(24 * time.Hour).Unix(),
+	})
+
+	signedAccToken, err := accToken.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
+	if err != nil {
+		return nil, err
+	}
+	signedRefToken, err := refToken.SignedString([]byte(os.Getenv("JWT_SECRET_KEY")))
+	if err != nil {
+		return nil, err
+	}
+
+	header := map[string]string{
+		"access_token":  signedAccToken,
+		"refresh_token": signedRefToken,
+	}
+
+	md := metadata.New(header)
+
+	ctx = metadata.NewOutgoingContext(ctx, md)
+
+	err = grpc.SetHeader(ctx, md)
+	if err != nil {
+		return nil, err
+	}
+
+	return &authpb.LoginResponse{
+		Message: "success login",
+	}, nil
 }
